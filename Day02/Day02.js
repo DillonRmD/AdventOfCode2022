f18792f414a240960f8e5863affc809542680118
DillonRmD <dillonwilliams2001@yahoo.com>
2022-12-02 16:38:04 -0600

    Finsihed day 2

--- Day02/Day02.js
+++ Day02/Day02.js
@@ -1,19 +1,56 @@
 const fs = require('fs');
 
-let data = fs.readFileSync('Day02Input.txt', 'utf8');
-const arr = data.split('\n');
+let data = fs.readFileSync('E:\\dev\\AdventOfCode2022\\Day02\\Day02Input.txt', 'utf8');
+const arr = data.split('\r\n');
 console.log(arr);
 
-const ScoreMap = 
-{
-    "Win" : 6,
-    "Draw": 3,
-    "Loss": 0, 
-};
 
-const ShapeMap = 
-{
-    "Y": 1,
-    "X": 2,
-    "Z": 3
-};
+//A : Rock
+//B : Paper
+//C : Scissors
+
+// Rock, => 1 pt
+// Paper => 2 pt
+// Scissors => 3 pt
+
+//X: Lose
+//Y: Draw
+//Z: Win
+
+//Win => 6 pts
+//Draw => 3 pts
+//Loss => 0 pts
+
+let score = 0;
+arr.forEach(round => {
+
+    let oppChoice = round.charAt(0);
+    let playerChoice = round.charAt(2);
+    
+    if (oppChoice == 'A') { // rock
+        if (playerChoice == 'X') // lose
+            score += (0 + 3); //scissors => 3pts
+        else if (playerChoice == 'Y') // draw
+            score += (3 + 1); //rock => 1pt
+        else if (playerChoice == 'Z') // win
+            score += (6 + 2); //paper => 2pt
+    }
+    else if (oppChoice == 'B') { // paper
+        if (playerChoice == 'X') //lose
+            score += (0 + 1); //rock => 1pt
+        else if (playerChoice == 'Y') //draw
+            score += (3 + 2); //paper => 2pt
+        else if (playerChoice == 'Z') //win
+            score += (6 + 3); //scissors => 3pt
+    }
+    else if (oppChoice == 'C') { //scissors
+        if (playerChoice == 'X') //lose
+            score += (0 + 2); //paper => 2pts
+        else if (playerChoice == 'Y') //draw
+            score += (3 + 3); //scissors => 3pts
+        else if (playerChoice == 'Z') // win
+            score += (6 + 1); //rock => 1pt
+    }
+})
+
+console.log(score);

727408a9d55192b2ec318d425525f43886c7174f
Dillon Williams <dillonwilliams2001@yahoo.com>
2022-12-05 08:41:07 -0600

    Finished part2 Day03

--- Day03/Day03.js
+++ Day03/Day03.js
@@ -1,15 +1,13 @@
 const fs = require('fs');
 
-let data = fs.readFileSync('E:\\dev\\AdventOfCode2022\\Day03\\Day03Input.txt', 'utf8');
-const arr = data.split('\r\n');
-console.log(arr);
+let data = fs.readFileSync('Day03Input.txt', 'utf8').replace(/\r/g, "").trim().split('\n');
 
 //lowercase items => a - z => 1 - 26 priority
 //uppercase items => A - Z => 27 - 52 priority
 
 const calculateValue = (char) => char.charCodeAt(0) - (char === char.toLowerCase() ? 96 : 38);
 
-function solve(data){
+function solve1(data){
     return data.split(/(.+\n.+\n.+)/g).map(x => x.trim()).filter(x => x.length > 0).map(x => x.split('\n')).reduce((sum, group) => {
 		const firstIndex = group.reduce((firstIndex, line, index) => line.length < group[firstIndex].length ? index : firstIndex, 0);
 		const [second, third] = [0, 1, 2].filter(i => i !== firstIndex).map(i => new Set(group[i]));
@@ -17,5 +15,23 @@ function solve(data){
 		return sum + calculateValue(commonItem);
 	}, 0)
 }
+function solve2(arr){
+	let sum = 0;
+	for(let i = 0; i < arr.length; i += 3)
+	{
+		const backpacks = [[...arr[i]], [...arr[i + 1]], [...arr[i + 2]]];
 
-console.log(solve(data));
+		let set = new Set(backpacks[0]);
+		let intersection = backpacks[1].filter((x) => set.has(x));
+
+		set = new Set(intersection);
+		intersection = backpacks[2].filter((x) => set.has(x));
+
+		const dedup = [...new Set(intersection)];
+
+		sum += calculateValue(dedup[0]);
+	}
+	return sum;
+}
+
+console.log(solve2(data));
